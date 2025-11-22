ee23ec15a1dc5b1bf30e3abaeea95788161fca17
Ruben Zilibowitz <ruben@rubenz.net>
2025-11-22 15:14:02 +1100

    Initial Commit

---
+++ SimpleDiary/SimpleDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDiaryApp.swift
+//  SimpleDiary
+//
+//  Created by Ruben Zilibowitz on 22/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
